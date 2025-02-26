0ae4c2404388c0c89175298a0464a9e0ec2a9455
Mitlit <albatrosstebex@gmail.com>
2025-02-26 17:35:26 +1300

    [1.0.2] - 2025-02-26 List of changes for the 1.0.2 release.
    ### Added
    - **Relative Damage**: Added a new feature to calculate relative damage.
      - **Relative Damage Rounding**: Rounds relative damage to the nearest whole number.
    - **Damage Effect**: Added a new feature to display the usual damage effect.
    - **Changelog**: Added a changelog file to keep track of changes.
    - **bStats**: Added bStats to track plugin usage statistics.
    
    ### Fixed
    - **Health Surplus**: Fixed a bug where the plugin would attempt to increase player health above **20 points** and cause an error output in console.

--- src/main/java/it/mitl/healthSync/HealthSync.java
+++ src/main/java/it/mitl/healthSync/HealthSync.java
@@ -1,6 +1,8 @@
 package it.mitl.healthSync;
 
 import it.mitl.healthSync.Listeners.Syncing;
+import org.bstats.bukkit.Metrics;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HealthSync extends JavaPlugin {
@@ -8,6 +10,15 @@ public final class HealthSync extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        // Configuration Setup
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+
+        // bStats
+        int pluginId = 24929;
+        Metrics metrics = new Metrics(this, pluginId);
+
         this.getServer().getPluginManager().registerEvents(new Syncing(this), this);
     }
 
